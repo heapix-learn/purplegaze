b2d3a91b99514330f40034e2a95e031625f6a292
RRuslanIv <r.rusachenka@gmail.com>
2018-12-08 14:04:57 +0300

    feat: add comments

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -23,23 +23,40 @@
           </p>
         </div>
         <div class="card-action">
-          <a href="#" class="card-action-icons"><i class="medium material-icons">call_missed</i></a>
-          <a href="#" class="card-action-icons"><i class="medium material-icons">favorite</i></a>
-          <a href="#" class="card-action-icons"><i class="medium material-icons">exposure_neg_1</i></a>
+          <a class="card-action-icons"><i class="medium material-icons">call_missed</i></a>
+          <a class="card-action-icons"><i class="medium material-icons">favorite</i></a>
+          <a class="card-action-icons"><i class="medium material-icons">exposure_neg_1</i></a>
+          <a class="card-action-icons comment" @click="showComments()"><i class="medium material-icons">comment</i></a>
         </div>
+        <Comments v-if="isComments" :message_id="message.id"></Comments>
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Comments from '../components/comments/Comments.vue'
 
 export default {
   name: 'Message',
+  data () {
+    return {
+      isComments: false
+    }
+  },
+  components: { Comments },
   props: {
     message: Object
   },
   methods: {
+    showComments () {
+      if (this.isComments) {
+        this.isComments = false
+      } else {
+        this.isComments = true
+      }
+    },
     isHash (tag) {
       if (tag.length > 0) {
         return true
@@ -82,7 +99,11 @@ export default {
   }
 
   .card-action-icons {
-    margin: 0;
+    margin: 0 !important;
+  }
+
+  .comment {
+    cursor: pointer;
   }
 
 </style>
---
+++ src/components/comments/Comments.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div v-for="(comment, index) in comments" :key="index" class="commentList">
+      <span class="commentList__author">{{comment.author}}</span> : {{comment.text}}
+    </div>
+    <div class="create-area">
+      <input class="create-area__input"
+             type="text"
+             v-model="newComment.text"
+             @input="clearInput"
+      >
+      <a class="card-action-icons" @click="createComment()"><i class="medium material-icons create-area__btn">chat_bubble</i></a>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Comments',
+  props: {
+    message_id: Number
+  },
+  data () {
+    return {
+      newComment: {
+        message_id: Number,
+        author: '',
+        text: ''
+      },
+      comments: []
+    }
+  },
+  created () {
+    this.getComments()
+  },
+  methods: {
+    async getComments () {
+      await axios.get('http://localhost:8008/comments?message_id=' + this.message_id)
+        .then(response => {
+          this.comments = response.data
+        })
+    },
+    async createComment () {
+      if (this.newComment.text.length > 1) {
+        this.newComment.message_id = this.message_id
+        this.newComment.author = this.$store.getters['user/user'].firstName + ' ' + this.$store.getters['user/user'].lastName
+        await axios.post('http://localhost:8008/comments', this.newComment)
+        this.newComment.text = ''
+        this.getComments()
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .create-area {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  .create-area__input {
+    width: 65% !important;
+    padding: 0 1rem !important;
+    margin-bottom: 5px;
+  }
+
+  .create-area__btn {
+    color: orange;
+  }
+
+  .commentList {
+    padding-left: 3rem;
+  }
+
+  .commentList__author {
+    font-weight: bold;
+  }
+</style>
